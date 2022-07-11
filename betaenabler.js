62e56a4a5e5217c6f8f3d0dfcbe5b4ddf11e525b
pu3blo <95724645+pu3blo@users.noreply.github.com>
2022-07-11 21:59:21 +0200

    Update betaenabler.js

--- betaenabler.js
+++ betaenabler.js
@@ -1,1 +1,2 @@
+// Enable a secret menu in discord to enable beta tools
 Object.defineProperty((webpackChunkdiscord_app.push([[''],{},e=>{m=[];for(let c in e.c)m.push(e.c[c])}]),m).find(m=>m?.exports?.default?.isDeveloper!==void 0).exports.default,"isDeveloper",{get:()=>true});
